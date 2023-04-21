cf4c9af8beb23cc304870522b0ff0dfcd0ce57f9
Richona <105828822+Richona@users.noreply.github.com>
2023-04-21 20:17:49 -0300

    agregue java vacio

---
+++ practicaJava/src/practicajava/PracticaJava.java
@@ -0,0 +1,19 @@
+package practicajava;
+import java.util.Random;//numeros aleatorios.
+import javax.swing.JOptionPane;//entrada y salida de datos en modal.
+import java.util.Scanner;
+
+public class PracticaJava {
+    /*
+        
+    */
+    static Random rand = new Random();
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+
+
+    }
+
+}
