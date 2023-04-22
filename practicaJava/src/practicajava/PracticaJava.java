3f197b6b098ab9551f6aa976d279733f7444cef0
maxsangre <max_sangre@live.com.ar>
2023-04-21 21:23:35 -0300

    cambio

--- practicaJava/src/practicajava/PracticaJava.java
+++ practicaJava/src/practicajava/PracticaJava.java
@@ -12,8 +12,35 @@ public class PracticaJava {
 
     public static void main(String[] args) {
         
-
-
+  Scanner leer = new Scanner(System.in);
+                System.out.println("Ingrese un numero: ");
+                int num = leer.nextInt();
+            if(num==1 || num==0){
+                System.out.println("Deja de joder");
+            }else{
+                 if(conversorDePrimos(num) == true){
+                     System.out.println("Tu numero es primo");
+                 
+            }else{
+                     System.out.println("Tu numero NO es primo");
+                 }
+                
+               
+                
+            } 
     }
+public static boolean conversorDePrimos(int num){ 
+for (int i=2 ; i<num ; i++) {
+if(num%i==0)
+return false;
+}
+return true;
+}
 
 }
+    
+
+
+    
+
+
