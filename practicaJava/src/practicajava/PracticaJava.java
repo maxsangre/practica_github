da687905e935d72b4f8a199245f49c9356b2fa7b
Richona <105828822+Richona@users.noreply.github.com>
2023-04-21 21:16:09 -0300

    agregue ejer

--- practicaJava/src/practicajava/PracticaJava.java
+++ practicaJava/src/practicajava/PracticaJava.java
@@ -11,7 +11,13 @@ public class PracticaJava {
     static Scanner leer = new Scanner(System.in);
 
     public static void main(String[] args) {
+       String pal; 
+        char matriz[][];
+        boolean espDisp[];
+        int posAlea, colAlea, acu;
         
+        matriz = new char [20][20];
+        espDisp = new boolean[20];
 
 
     }
